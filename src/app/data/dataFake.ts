20fd1800a9dfc8673f095b27c9c2d37d17616c80
Rafael <rafaelrgsenhorinho@gmail.com>
2023-06-02 20:53:22 -0300

    routing

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"",
+        "title":"Look for the new Spider-Man",
+        "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        "photoCover":"https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=1200&height=750&format=webp&quality=91&imagick=uploads.adrenaline.com.br/2023/05/Marvels-Spider-Man-2-foi-desenvolvido-sem-concessoes-afirma-Sony.jpg"
+    },
+    {
+        "id":"2",
+        "photo":"",
+        "title":"Playstation announce the new game",
+        "description":"What do you think about this new cast?",
+        "photoCover":"https://gamehall.com.br/wp-content/uploads/2022/07/spider-man-pc.jpg"
+    },
+    {
+        "id":"3",
+        "photo":"",
+        "title":"The trailer was finally announced",
+        "description":"What do you think about this new cast?",
+        "photoCover":"https://meups.com.br/wp-content/uploads/2021/09/Trailer-de-Marvels-Spider-Man-2.jpg"
+    },
+    {
+        "id":"4",
+        "photo":"",
+        "title":"A strange Peter takes place",
+        "description":"What do you think about this new cast?",
+        "photoCover":"https://meups.com.br/wp-content/uploads/2023/05/Marvels-Spider-Man-2-1-900x503.jpg"
+    }
+]
